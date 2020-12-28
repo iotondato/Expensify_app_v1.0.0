87860e78a4856e8187c504f583b0ecc32c32390b
bretondato <brenno.tondato@gmail.com>
2020-12-28 01:45:57 -0300

    version of deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,51 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports ={
-    // entry: "./src/playground/hoc.js",
-    entry: "./src/app.js",
-    output:{
-        path:path.join(__dirname, 'public'),
-        filename:"bundle.js"
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  // const CSSExtract = new ExtractTextPlugin('styles.css');
+
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
     },
-    module:{
-        rules:[{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-            'style-loader', 
-            'css-loader',
-            'sass-loader'
-            ]
-        }]    
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },{
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }]
     },
-    devtool: 'cheap-module-eval-source-map',
-    devServer:{
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'styles.css',
+        ignoreOrder: false, // Enable to remove warnings about conflicting order
+      })
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
     }
-}
+  };
+};
